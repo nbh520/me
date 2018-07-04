aa24ce37da38ab6353b2cff1b4292579d18ce820
nibohan <154524331@qq.com>
2018-07-04 22:58:14 +0800

    更新了一些首页

---
+++ js/Star.js
@@ -0,0 +1,59 @@
+/*
+ * 
+ * 流星划过
+ */
+
+var BodyWidth = document.body.offsetWidth;
+var BodyHeight = document.body.offsetHeight;
+var StarDom = document.getElementById("star");
+
+/*
+ * 星星对象，x,y坐标，宽度，长度，移动速度，透明值，生成dom元素
+ */
+function Star() {
+	this.x = this.randomNum(0, BodyWidth * 2);
+	this.y = BodyHeight + this.randomNum(0, BodyHeight);
+	this.width = this.randomNum(15, 64);
+	this.height = this.width / 64 * 143;
+	this.speed = this.randomNum(3, 5);
+	this.k = this.y - (this.height / this.width) * this.x + (this.width * this.height / this.width);
+	this.opacity = this.randomNum(0.5, 1);
+	this.dom = this.createNode();
+}
+Star.prototype = {
+	createNode: function() {
+		var img = new Image();
+		img.src = "img/liuxing.png";
+		img.width = this.width;
+		img.height = this.height;
+		img.style.left = this.x + "px";
+		img.style.top = this.y + "px";
+		img.style.opacity = this.opacity;
+		img.className = "star-position";
+		StarDom.appendChild(img);
+		return img;
+	},
+	randomNum: function(min, max) {
+		return Math.random() * (max - min) + min;
+	}
+}
+var StarArr = [];
+for(let i = 0; i < 50; i++) { //生成若干星星
+	StarArr[i] = new Star();
+}
+/*
+ * 
+ * 每次刷新都重新对星星坐标进行绘制
+ */
+setInterval(function() {
+	for(let i = 0; i < StarArr.length; i++) {
+		if(StarArr[i].dom.offsetTop <= -(StarArr[i].height) ||
+			StarArr[i].dom.offsetLeft <= -(StarArr[i].width)) {
+			StarDom.removeChild(StarArr[i].dom);
+			StarArr[i] = new Star();
+		} else {
+			StarArr[i].dom.style.top = StarArr[i].k - 1 + 'px';
+			StarArr[i].dom.style.left = -1 * StarArr[i].width - 1 + 'px';
+		}
+	}
+}, 1000);
--- js/scroll.js
+++ js/scroll.js
@@ -1,31 +1,28 @@
 (function() {
 	var box = document.querySelector('.wrap'); //获取整个大盒子
 	var child = box.children;  // 获取每个分页HTMLcollection
-	var i = 0;
-	console.log(child.length)
+	var page = 0;
+	var startTime = 0;
 	
 	//滚动条事件
 	Array.prototype.forEach.call(child, function(el, index) {
 		window.onmousewheel = function(e){
-			if(e.wheelDelta > 0 && i < 0){
-				i++;
-				box.style.top = i  + "00%";
-			}else if(e.wheelDelta < 0 && i > -4){
-				i--;
-				box.style.top = i + "00%";
+			var now = new Date();
+			overtTime = now.getTime();
+			if(overtTime - startTime > 1000){   // 设置滚动间隔的时间
+				if(e.wheelDelta > 0 && page < 0){
+					page++;
+					box.style.top = page  + "00%";
+				}else if(e.wheelDelta < 0 && page > -4){
+					page--;
+					box.style.top = page + "00%";
+				}
+				startTime = now.getTime();
 			}
+			
 			
 		}
 	})
 	
-	//自定义停止几秒
-	function sleep(numberMillis){
-		var now = new Date(); 
-		var exitTime = now.getTime() + numberMillis;
-		while(true){
-			now = new Date();
-			if(now.getTime() > exitTime)
-				return;
-		}
-	}
+	
 })()
