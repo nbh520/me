2a44fa8a57dd201d01348c58771ae87a303dc305
nibohan <154524331@qq.com>
2018-07-05 08:11:46 +0800

    更新

--- js/scroll.js
+++ js/scroll.js
@@ -1,19 +1,20 @@
 (function() {
 	var box = document.querySelector('.wrap'); //获取整个大盒子
-	var child = box.children;  // 获取每个分页HTMLcollection
-	var page = 0;
-	var startTime = 0;
-	
+	var boxChild = box.children;  // 获取每个分页HTMLcollection
+	var page = 0;  //当前所在页面
+	var pageCount = boxChild.length
+	var startTime = 0;  //时间间隔初始值
 	//滚动条事件
-	Array.prototype.forEach.call(child, function(el, index) {
+	Array.prototype.forEach.call(boxChild, function(el, index) {
 		window.onmousewheel = function(e){
 			var now = new Date();
 			overtTime = now.getTime();
-			if(overtTime - startTime > 1000){   // 设置滚动间隔的时间
+			//设置滚动的间隔时间，但首页和尾页不设置间隔
+			if(overtTime - startTime > 1000 || page == 0 || page == -1 * pageCount + 1){   
 				if(e.wheelDelta > 0 && page < 0){
 					page++;
 					box.style.top = page  + "00%";
-				}else if(e.wheelDelta < 0 && page > -4){
+				}else if(e.wheelDelta < 0 && page > -1 * pageCount + 1){
 					page--;
 					box.style.top = page + "00%";
 				}
